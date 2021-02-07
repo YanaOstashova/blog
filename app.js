ca903ee998415ccc22271a27b2f65ba836772b0c
YanaOstashova <YanaOstashova17@gmail.com>
2021-02-07 22:00:43 +0300

    create css

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const fs = require("fs");
+var path = require('path');
 
 const app = express();
 const port = 2000;
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "hbs");
 
 app.listen(port, () => {
@@ -11,7 +12,7 @@ app.listen(port, () => {
 });
 
 app.get("/", (request, response) => {
-    const file = fs.readFileSync("./data/notes.json");
+    const file = fs.readFileSync("data/notes.json");
 
     const model = JSON.parse(file);
     response.render("index", model);
@@ -19,13 +20,44 @@ app.get("/", (request, response) => {
 
 app.get("/info/:id", (request, response) => {
     const id = request.params.id;
-    const file = fs.readFileSync("./data/notes.json");
+    const file = fs.readFileSync("data/notes.json");
     const model = JSON.parse(file);
     const notes = model.notes.filter(x => x.id == id);
 
     if (notes.length < 1) response.redirect("/");
-
     response.render("info", notes[0]);
+});
+
+app.get("/admin/edit/:id", (request, response) => {
+    const id = request.params.id;
+    const file = fs.readFileSync("data/notes.json");
+    const model = JSON.parse(file);
+    const notes = model.notes.filter(x => x.id == id);
+    let note = {};
+    if (notes.length > 0) note = notes[0];
+
+    response.render("info", note);
+});
+
+app.post("/admin/edit", (request, response) => {
+    const editInfo = request.body;
+    if (!editInfo) response.redirect("/");
+
+    const file = fs.readFileSync("data/notes.json");
+    const model = JSON.parse(file);
+    const notes = model.notes.filter(x => x.id == id);
+
+    if (notes.length < 1) {
+        const note = notes[0];
+
+        note.title = editInfo.title;
+        note.text = editInfo.text;
+    } else {
+        note.id = notes[notes.length - 1].id + 1;
+        notes.push(note);
+    }
+
+    response.redirect("/");
 });
 
 app.get("/*", (request, response) => {
